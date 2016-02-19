06f3755fe8995db47bf4e9b8285abbd12b772975
Daniel Klopp <dsklopp@gmail.com>
2016-02-19 10:50:21 -0800

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'kafka-cluster'
+maintainer       'Daniel Klopp'
+maintainer_email 'see github'
+license          'All rights reserved'
+description      'Installs/Configures kafka-cluster'
+long_description 'Installs/Configures kafka-cluster'
+version          '0.1.0'
+
+
